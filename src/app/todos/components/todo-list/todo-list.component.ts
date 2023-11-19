16b146c548f937c8166909f482347ef34fa65e69
Michal Kaminski <MichalKaminski.KamienKaze@gmail.com>
2023-11-19 02:34:54 +0100

    task list without api calls

--- src/app/todos/components/todo-list/todo-list.component.ts
+++ src/app/todos/components/todo-list/todo-list.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectedUserDataService } from '../../../shared/services/selected-user-data.service';
+import { Todo } from '../../../shared/types/todo';
+import { TodoComponent } from '../todo/todo.component';
+import { MaterialModule } from '../../../material/material.module';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodoComponent, MaterialModule],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrl: './todo-list.component.scss',
 })
 export class TodoListComponent {
+  public todoList: Todo[] = [];
 
+  constructor() {
+    SelectedUserDataService.selectedUserTodos.subscribe(
+      (next: Todo[]): void => {
+        this.todoList = next;
+      },
+    );
+  }
 }
---
+++ src/app/todos/components/todo/todo.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../../material/material.module';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss',
+})
+export class TodoComponent {
+  @Input('completed') public completed: boolean = false;
+  @Input('title') public title: string = 'New Task';
+  @Input('todoId') private id: number = 0;
+}
